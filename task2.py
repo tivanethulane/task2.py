71e6e7950f4706bf1ce24660abdca7f2993a3dd6
thulane <111046874+tivanethulane@users.noreply.github.com>
2023-01-31 19:34:02 -0330

    Add files via upload

--- task2.py
+++ task2.py
@@ -1,5 +1,15 @@
-#name = input()
 def task2(name):
     print(f"Hello {name}!")
 
+
 task2("Sally")
+def task2(name):
+    print(f"Hello {name}!")
+
+
+task2("Tshepo") 
+def task2():
+    print(f"Hello Friend!")
+
+
+task2()    
